99b60aa27301a8ade93ad90663363c5991cc7a47
Trond Bordewich <trond@bswmail.no>
2018-09-14 14:39:21 +0200

    Updated bootstrap, started with client creation page

---
+++ Pages/Admin/ClientSelection.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Authserver.Pages.Admin
+{
+    public class ClientSelectionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
